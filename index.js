ecabea9e313d0185e2a2b256427a9a45e2ffc973
Craig Wilson <cw66786@gmail.com>
2022-09-27 20:19:17 -0500

    added media query and formatted

--- index.js
+++ index.js
@@ -9,42 +9,37 @@ const data = [
   { region: "CA", model: "B", sales: 100 },
   { region: "CA", model: "C", sales: 230 },
   { region: "CA", model: "D", sales: 400 },
-  
 ];
 
 const table = document.getElementById("table");
 
+let sums = {};
 
-let sums = {}
+data.forEach((el) => {
+  if (sums[el.rlgion]) {
+    sums[el.region] = sums[el.region] + el.sales;
+  } else {
+    sums[el.region] = el.sales;
+  }
+});
 
-data.forEach(e => {
-    if(sums[e.region]) {
-        sums[e.region] = sums[e.region]+e.sales
-    } else {
-        sums[e.region] = e.sales
-    }
-})
-
-
-const setTableData = (d,sum) => {
-  for (let el in d) {
-    let region = d[el].region;
+const setTableData = (d, sum) => {
+  d.forEach((el) => {
+    let region = el.region;
 
     let newChild = `<tbody class="${region}">
                     <tr class="sumRow"><td>${region}</td><td>Sum</td><td>${sum[region]}</td></tr></tbody>`;
 
-    let newRow = `<tr><td>${region}</td><td>${d[el].model}
-                    </td><td>${d[el].sales}</td</tr>`;
+    let newRow = `<tr><td>${region}</td><td>${el.model}
+                    </td><td>${el.sales}</td</tr>`;
 
-                    
     if (table.getElementsByClassName(region).length === 0) {
       table.innerHTML += newChild;
       table.getElementsByClassName(region)[0].innerHTML += newRow;
     } else {
       table.getElementsByClassName(region)[0].innerHTML += newRow;
     }
-  }
+  });
 };
 
-
-setTableData(data,sums);
+setTableData(data, sums);
